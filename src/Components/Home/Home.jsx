edca64084246cb57a12749c8a7cd9ed7caf48197
Sadek Ali <sadekali1530@gmail.com>
2023-09-29 15:10:25 +0600

    add the code

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
-            <h1>This is home...........</h1>
+           <Link to={"/"}>Home</Link>
+           <Link to={"/login"}>Login with google</Link>
         </div>
     );
 };
